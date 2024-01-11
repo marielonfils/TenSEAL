184c1055e80892c31e4fa0c1394de7324e423d94
marielonfils <marie.lonfils@uclouvain.be>
2024-01-11 11:48:18 +0100

    link to seal repo

--- cmake/seal.cmake
+++ cmake/seal.cmake
@@ -6,8 +6,9 @@ set(SEAL_USE_INTEL_HEXL OFF)
 
 FetchContent_Declare(
   com_microsoft_seal
-  GIT_REPOSITORY https://github.com/marielonfils/SEAL  #https://github.com/microsoft/SEAL
-  GIT_TAG        v4.1.1
+  #SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../SEAL
+  GIT_REPOSITORY https://github.com/marielonfils/SEAL
+  GIT_TAG        v4.1.1-mk_ckks
 )
 FetchContent_MakeAvailable(com_microsoft_seal)
 
